d724a45b047a2691a19886652aafdeb52925009a
tracysyd <tracysyd@users.noreply.github.com>
2023-11-23 21:26:18 +1100

    Update streamlit_app.py
    excute a sql

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,6 @@ streamlit.dataframe(my_data_rows)
 # Allow the end user to add fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+# This will not work correctly
+my_cur.excute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
